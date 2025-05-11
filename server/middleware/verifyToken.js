6c3dc47ae3fe24d94d19b8ab2dfa751ef3f0c565
jitendra yadav <jitendrayadav200317@gmail.com>
2025-05-11 14:39:19 +0530

    fix error

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,21 +1,29 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const verifyToken = (req, res, next) => {
- 
   const token = req.cookies.token;
   console.log(token);
+
   if (!token) {
     return res.status(401).json({
       authenticated: false,
-      message: 'No token found',
+      message: "No token found",
     });
   }
 
-  const decoded = jwt.verify(token, 'hello_this_string');
-  req.user = decoded;
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({
+      authenticated: false,
+      message: "Invalid token",
+    });
+  }
 };
 
 export default verifyToken;
-
-//parse cookie-parser
